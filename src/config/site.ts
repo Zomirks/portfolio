278f154f9589733b4e966943b363db13a4bdf8d4
Cyril Fischer <github@cyril-fischer.fr>
2026-04-10 15:51:55 +0200

    feat: add site config

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+	name: "Cyril Fischer",
+	title: "Développeur Fullstack",
+	location: "Strasbourg, Alsace, France",
+	socials: [
+		{ label: "Github", href: "https://github.com/Zomirks" },
+		{ label: "LinkedIn", href: "https://www.linkedin.com/in/cyril-fischer/" },
+	],
+} as const;
