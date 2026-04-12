85010974ee338ff8573838aa7033a0810028c3e6
Cyril Fischer <github@cyril-fischer.fr>
2026-04-12 15:46:42 +0200

    feat: improve BackToTop btn

--- src/components/ui/back-to-top.tsx
+++ src/components/ui/back-to-top.tsx
@@ -1,12 +1,15 @@
-import Link from 'next/link';
+'use client';
+
 import { IconChevronsUp } from '@tabler/icons-react';
 
-export default function backToTop() {
+export default function BackToTop() {
 	return (
-		<Link 
-			href='/#top'
-			className="fixed bottom-0 right-4 size-12 bg-gray-50 rounded-t-lg flex items-center justify-center">
+		<button 
+			onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
+			className="fixed bottom-0 right-4 size-12 bg-gray-50 rounded-t-lg flex items-center justify-center cursor-pointer"
+			aria-label="Retour en haut"
+		>
 			<IconChevronsUp color='black'/>
-		</Link>
+		</button>
 	)
 }
