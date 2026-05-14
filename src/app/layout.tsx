d33ecb03591590d63fb4efa0716f511cfd6fbca6
Cyril Fischer <19796006+Zomirks@users.noreply.github.com>
2026-05-14 22:01:46 +0200

    feat: respect Nextjs 16 guidelines

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,16 +18,18 @@ export const metadata: Metadata = {
 	description: siteConfig.description,
 	metadataBase: new URL(siteConfig.url),
 	alternates: { canonical: "/" },
-	openGraph: { type: "website", locale: "fr_FR", url: siteConfig.url, description: siteConfig.description, images: [{
-		url: "/opengraph-image.jpg",
-		width: 1200,
-		height: 630
-	}], siteName: siteConfig.name},
+	openGraph: {
+		title: `${siteConfig.name} | ${siteConfig.job}`,
+		type: "website",
+		locale: "fr_FR",
+		url: siteConfig.url,
+		description: siteConfig.description,
+		siteName: siteConfig.name
+	},
 	twitter: {
 		card: "summary_large_image",
 		title: `${siteConfig.name} | ${siteConfig.job}`,
 		description: siteConfig.description,
-		images: ["/opengraph-image.jpg"],
 	},
 	authors: [{ name: siteConfig.name, url: siteConfig.url }],
 	creator: siteConfig.name,
