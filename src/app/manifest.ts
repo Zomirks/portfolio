94af8e52ff4a584679382b2d491f5a0d2ada4bb6
Cyril Fischer <19796006+Zomirks@users.noreply.github.com>
2026-05-16 15:34:02 +0200

    feat: add manifest & Favicon

---
+++ src/app/manifest.ts
@@ -0,0 +1,31 @@
+import type { MetadataRoute } from "next";
+import { siteConfig } from "@/config/site";
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: `${siteConfig.name} | ${siteConfig.job}`,
+		short_name: siteConfig.name,
+		description: siteConfig.description,
+		start_url: '/',
+		scope: '/',
+		display: 'standalone',
+		dir: 'ltr',
+		lang: 'FR',
+		background_color: '#1a1818',
+		theme_color: '#1a1818',
+		icons: [
+			{
+				"src": "/web-app-manifest-192x192.png",
+				"sizes": "192x192",
+				"type": "image/png",
+				"purpose": "maskable"
+			},
+			{
+				"src": "/web-app-manifest-512x512.png",
+				"sizes": "512x512",
+				"type": "image/png",
+				"purpose": "maskable"
+			}
+		],
+	};
+}
