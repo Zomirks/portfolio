90c7c5b81227651831f0c37ddabcc34973592d13
Cyril Fischer <19796006+Zomirks@users.noreply.github.com>
2026-05-20 14:33:08 +0200

    feat: add "scroll" mention to Hero component

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -1,5 +1,4 @@
-// import { ChevronDown } from "lucide-react";
-import { IconDownload } from "@tabler/icons-react";
+import { ChevronsDownIcon } from "lucide-react";
 import { siteConfig } from "@/config/site";
 
 import { DotPattern } from "@components/ui/dot-pattern";
@@ -39,10 +38,10 @@ export default function Hero() {
 						<SocialsList className="items-center justify-center space-x-6 mt-8" />
 					</div>
 				</div>
-				{/* <div className="flex flex-col items-center">
-					<div className="uppercase">Scroll</div>
-					<ChevronDown className="animate-bounce"/>
-				</div> */}
+				<div className="flex flex-col items-center font-fraunces text-foreground-muted">
+					<div className="font-light mb-2 uppercase">Scroll</div>
+					<ChevronsDownIcon className="animate-bounce"/>
+				</div>
 
 				<DotPattern 
 					glow={true}
